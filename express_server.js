c588f144d3b1db3885342e91d3b69ce9076c3a62
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-22 23:12:58 +0000

    was able to get deletion of urls to work using post

--- express_server.js
+++ express_server.js
@@ -72,6 +72,13 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+// Delete POST /urls/:shortURL/delete
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+})
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
